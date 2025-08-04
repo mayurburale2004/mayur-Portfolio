27028f3ff5a9afe33d3ca53ad1a4dbed45d1d929
Mayur Burale <mayurburale@gmail.com>
2025-08-04 15:34:47 +0530

    Update resume

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -43,7 +43,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1YkkWb_6VI-RjDR3MI-ZDABSHUcs22HEF/view?usp=sharing"
+            href="https://drive.google.com/file/d/10yILQZyWvVLWdWfHHPGqCR2s8vgy67uT/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
