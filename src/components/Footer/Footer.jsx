35d6147c9e4720bd6dcbea9e61da990d87c87b2b
Mayur93077 <mayurburale@gmail.com>
2026-03-14 15:54:56 +0530

    trigger vercel redeploy

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -48,7 +48,7 @@ const Footer = () => {
 
         {/* Copyright Text */}
         <p className="text-sm text-gray-400 mt-6">
-          © 2026 Mayur Burale. All rights reserved.
+          © 2025 Mayur Burale. All rights reserved.
         </p>
       </div>
     </footer>
